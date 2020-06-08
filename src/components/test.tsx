3b3a35693c6a75111d6c7ace4f39c16963796083
teddarific <teddyni19@gmail.com>
2020-06-07 23:09:01 -0700

    [web] Setting up initial config of TS and styling

---
+++ src/components/test.tsx
@@ -0,0 +1,8 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+
+export default function Test(){
+    return (
+        <div css={{ backgroundColor: 'black' }} > Hello </div>
+    )
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from 'next/app'
+import { CacheProvider, css, Global } from '@emotion/core'
+import { cache } from 'emotion'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <CacheProvider value={cache}>
+      <Global
+        styles={css`
+          html,
+          body {
+            margin: 0;
+            min-height: 100vh;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </CacheProvider>
+  )
+}
+
+export default App
