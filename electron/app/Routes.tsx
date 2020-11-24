60a93f0c2282d086203be4f51a53f635235d6452
alexdanilowicz <alex.danilowicz@gmail.com>
2020-11-23 23:44:39 -0800

    remove this

--- electron/app/Routes.tsx
+++ electron/app/Routes.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import routes from './constants/routes.json';
 import WordCountChart from './components/charts/WordCount';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path={routes.COUNTER} component={WordCountChart} />
+      <Route path={"/"} component={WordCountChart} />
     </Switch>
   );
 }
