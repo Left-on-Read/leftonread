cb5d1afa0c212c8ece48caf09dda02d37b875ca2
alexdanilowicz <alex.danilowicz@gmail.com>
2021-01-08 20:30:55 -0800

    everything

--- electron/src/index.tsx
+++ electron/src/index.tsx
@@ -1,13 +1,11 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { render } from 'react-dom';
-import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
 import * as sqlite3 from 'sqlite3';
 import log from 'electron-log';
 import './app.global.css';
 import WordCountChart from './components/charts/WordCount';
 import { coreInit } from './utils/initUtils';
-
-const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
 export default function Root() {
   const [db, setDB] = useState<sqlite3.Database | null>(null);
@@ -34,11 +32,11 @@ export default function Root() {
   return <div>Loading dash... </div>;
 }
 
-document.addEventListener('DOMContentLoaded', () =>
-  render(
-    <AppContainer>
-      <Root />
-    </AppContainer>,
-    document.getElementById('root')
-  )
+render(
+  <Router>
+    <Switch>
+      <Route path="/" component={Root} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
 );
