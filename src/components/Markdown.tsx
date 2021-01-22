180fc5b960c0244969c6c65c0e3a2cf388629e36
teddarific <teddyni19@gmail.com>
2021-01-21 23:18:00 -0800

    Adding framework of markdown pages

---
+++ src/components/Markdown.tsx
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import ReactMarkdown from 'react-markdown'
+import { Text } from './Text'
+
+const renderers = {
+  heading: ({ level, children }: { level: number; children: string }) => {
+    if (level === 1) {
+      return <Text type="display">{children}</Text>
+    }
+
+    return <Text type="header">{children}</Text>
+  },
+  paragraph: ({ children }: { children: string }) => {
+    return <Text type="paragraph">{children}</Text>
+  },
+  listItem: ({ children }: { children: string }) => {
+    return (
+      <li>
+        <Text type="paragraph">{children}</Text>
+      </li>
+    )
+  },
+}
+
+export function Markdown({
+  className,
+  raw,
+}: {
+  className?: string
+  raw: string
+}) {
+  return (
+    <ReactMarkdown className={className} renderers={renderers}>
+      {raw}
+    </ReactMarkdown>
+  )
+}
