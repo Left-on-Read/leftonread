1771b9955e0783779646c05eb7a6f5666f0b2d5a
alexdanilowicz <alex.danilowicz@gmail.com>
2021-01-25 23:56:29 -0800

    Add functionality for WordOrEmojiTypes

--- electron/src/chatBro/constants/reactions.ts
+++ electron/src/chatBro/constants/reactions.ts
@@ -1,4 +1,4 @@
-import { delimList } from '../../utils';
+import { delimList, lowerCaseList } from '../../utils';
 
 const reactionsList = ['Laughed', 'Loved', 'Emphasized', 'Disliked', 'Liked'];
-export const reactions = delimList(reactionsList);
+export const reactions = delimList(lowerCaseList(reactionsList));
--- electron/src/chatBro/queries/WordOrEmoji/filters.ts
+++ electron/src/chatBro/queries/WordOrEmoji/filters.ts
@@ -1,6 +1,11 @@
 import _ from 'lodash';
 import { emojis } from '../../constants/emojis';
 import { Columns } from './columns';
+import { GroupChatFilters } from '../../constants/filters';
+import { reactions } from '../../constants/reactions';
+import { stopWords } from '../../constants/stopWords';
+import { objReplacementUnicode } from '../../constants/objReplacementUnicode';
+import { punctuation } from '../../constants/punctuation';
 
 function isFromMeFilter(filters: WordOrEmojiTypes.Filters): string {
   if (filters.isFromMe === true) {
@@ -23,12 +28,31 @@ function isEmojiFilter(filters: WordOrEmojiTypes.Filters): string {
   } (${emojis})`;
 }
 
+function groupChatFilter(
+  filters: WordOrEmojiTypes.Filters
+): string | undefined {
+  if (filters.groupChat === GroupChatFilters.ONLY_INDIVIDUAL) {
+    return `${Columns.CACHE_ROOMNAMES} IS NULL`;
+  }
+  return undefined; // would query for both individual and groupchats
+}
+
+function fluffFilter(): string {
+  // NOTE: Text is LOWERed at this point.
+  return `TRIM(${Columns.WORD}) NOT IN (${stopWords})
+  AND TRIM(${Columns.WORD}) NOT IN (${reactions})
+  AND unicode(TRIM(${Columns.WORD})) != ${objReplacementUnicode}
+  AND TRIM(${Columns.WORD}) NOT IN (${punctuation})`;
+}
+
 export default function getAllFilters(
   filters: WordOrEmojiTypes.Filters
 ): string {
   const isEmoji = isEmojiFilter(filters);
   const isFromMe = isFromMeFilter(filters);
   const word = wordFilter(filters);
-  const filtersArray = _.compact([isFromMe, word, isEmoji]);
+  const groupChat = groupChatFilter(filters);
+  const fluff = fluffFilter();
+  const filtersArray = _.compact([isFromMe, word, isEmoji, groupChat, fluff]);
   return !_.isEmpty(filtersArray) ? `WHERE ${filtersArray.join(' AND ')}` : '';
 }
