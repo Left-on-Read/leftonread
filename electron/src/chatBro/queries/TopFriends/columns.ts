418f927a72100e131f53a5accdcd40672f4a5703
alexdanilowicz <alex.danilowicz@gmail.com>
2021-01-25 21:24:36 -0800

    change Columns to enums

--- electron/src/chatBro/queries/TopFriends/columns.ts
+++ electron/src/chatBro/queries/TopFriends/columns.ts
@@ -1,14 +1,14 @@
-export const Columns = {
-  COUNT: 'count',
-  PHONE_NUMBER: 'phone_number',
-  FRIEND: 'friend',
-  IS_FROM_ME: 'sent',
-  TEXT: 'text',
-};
+export enum Columns {
+  COUNT = 'count',
+  PHONE_NUMBER = 'phone_number',
+  FRIEND = 'friend',
+  IS_FROM_ME = 'sent',
+  TEXT = 'text',
+}
 
-export const OutputColumns = {
-  FRIEND: 'friend',
-  TOTAL: 'total',
-  SENT: 'sent',
-  RECEIVED: 'received',
-};
+export enum OutputColumns {
+  FRIEND = 'friend',
+  TOTAL = 'total',
+  SENT = 'sent',
+  RECEIVED = 'received',
+}
--- electron/src/chatBro/queries/WordOrEmoji/columns.ts
+++ electron/src/chatBro/queries/WordOrEmoji/columns.ts
@@ -1,8 +1,8 @@
 import { Columns } from '../../../tables/Core/Count';
 
-const OutputColumns = {
-  WORD: 'word',
-  COUNT: 'count',
-};
+enum OutputColumns {
+  WORD = 'word',
+  COUNT = 'count',
+}
 
 export { Columns, OutputColumns };
