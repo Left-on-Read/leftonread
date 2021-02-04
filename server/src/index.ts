2080a68e5fac6d59fdf2b7e94f288ec02f36ce74
teddarific <teddyni19@gmail.com>
2021-02-03 22:12:03 -0800

    Adding mongoose to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,34 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
 
 import router from './router'
 
 const app = express()
 app.use(bodyParser.json())
 
-const port = process.env.PORT || 9000
+const mongoURI =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/lorserver'
+
+mongoose.connect(
+  mongoURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log(`Connected to database: ${mongoURI}`)
+    }
+  }
+)
 
 app.use(router)
+
+const port = process.env.PORT || 9000
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
