75d02bea09d17ca61179d66ce2fd4c88ed25a789
teddarific <teddyni19@gmail.com>
2021-02-27 20:57:19 -0800

    Clean up chared eslint config

--- eslint-config/index.js
+++ eslint-config/index.js
@@ -3,17 +3,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
   },
   extends: [
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
