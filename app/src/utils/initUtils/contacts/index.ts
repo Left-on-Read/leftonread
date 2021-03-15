863064672e2868271254c70c2808063b0a248aee
Alexander Danilowicz <alex.danilowicz@gmail.com>
2021-03-14 18:13:15 -0700

    Order contact filter results by count, then email, then no matches (#105)

--- app/src/utils/initUtils/contacts/index.ts
+++ app/src/utils/initUtils/contacts/index.ts
@@ -7,19 +7,27 @@ export async function getContactOptions(
   db: sqlite3.Database
 ): Promise<ContactOptions.ContactData[]> {
   const q = `
-  SELECT
-    COALESCE(contact_name, h.id) as ${Columns.VALUE},
-    COALESCE(contact_name, h.id) as ${Columns.LABEL},
-    COUNT(*) as ${Columns.COUNT},
-    h.id as ${Columns.ID}
-  FROM message
-  JOIN handle h
-    ON
-      h.ROWID = handle_id
-  -- NOTE: don't bother showing robo-contacts,
-  -- i.e., Amazon shipping updates
-  WHERE LENGTH(h.id) >= ${PHONE_NUMBER_LENGTH}
-  GROUP BY h.id
-  ORDER BY ${Columns.COUNT} DESC`;
+    SELECT
+      COALESCE(contact_name, h.id) as ${Columns.VALUE},
+      COALESCE(contact_name, h.id) as ${Columns.LABEL},
+      COUNT(*) as ${Columns.COUNT},
+      h.id as ${Columns.ID}
+    FROM message
+    JOIN handle h
+      ON
+        h.ROWID = handle_id
+    -- NOTE: don't bother showing robo-contacts,
+    -- i.e., Amazon shipping updates
+    WHERE LENGTH(h.id) >= ${PHONE_NUMBER_LENGTH}
+    GROUP BY h.id
+    ORDER BY
+    CASE
+      -- NOTE: must use the COALESCE value here,
+      -- because all ids are phone numbers/emails
+      WHEN ${Columns.VALUE} LIKE '%@%' THEN -1
+      WHEN ${Columns.VALUE} GLOB '*[0-9]*' THEN -2
+      ELSE ${Columns.COUNT}
+    END DESC
+  `;
   return sqlite3Wrapper.allP(db, q);
 }
