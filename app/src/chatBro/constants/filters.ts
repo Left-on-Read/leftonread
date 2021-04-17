f32c64861f4856d322340543adb9a97cb7fffbb1
alexdanilowicz <alex.danilowicz@gmail.com>
2021-04-17 00:36:07 -0700

    Message

--- app/src/chatBro/constants/filters.ts
+++ app/src/chatBro/constants/filters.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_LIMIT = 15;
 
 export enum GroupChatFilters {
-  BOTH = 'Both Individual and Group Chats (will make received higher)',
+  BOTH = 'Both Individual and Group Chats (makes received higher & keeps sent the same)',
   ONLY_INDIVIDUAL = 'Only Individual Conversations',
 }
 
