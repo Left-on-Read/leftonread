79544d14fbbb1b6425d4cbdd5dfb2035cc6462b2
alexdanilowicz <alex.danilowicz@gmail.com>
2021-04-27 21:34:41 -0700

    Fix sql corrupt error by joining to core_count instead

--- app/src/components/filters/ContactFilter.tsx
+++ app/src/components/filters/ContactFilter.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import log from 'electron-log';
+import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
+import * as sqlite3 from 'sqlite3';
 
+import { getContactOptions } from '../../utils/initUtils/contacts';
 import { IContactData } from '../../utils/initUtils/contacts/types';
 
 interface ContactFilterProps {
-  options: IContactData[];
   handleChange: (selected?: IContactData | null | undefined) => void;
   contact: IContactData | null | undefined;
+  db: sqlite3.Database;
 }
 
 export default function ContactFilter(props: ContactFilterProps) {
-  const { contact, options, handleChange } = props;
+  const [contactOptions, setContactOptions] = useState<IContactData[]>([]);
+  const { contact, handleChange, db } = props;
+
+  useEffect(() => {
+    async function getContacts() {
+      try {
+        const allContacts = await getContactOptions(db);
+        setContactOptions(allContacts);
+      } catch (err) {
+        log.error('ERROR: fetching contact options', err);
+      }
+    }
+    getContacts();
+  }, [db]);
+
   return (
     <Select
       isClearable
       defaultValue={contact}
       onChange={handleChange}
-      options={options}
+      options={contactOptions}
     />
   );
 }
