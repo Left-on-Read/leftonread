bd9882a3158a28f4a3a473cdacc623a2d2ca43c3
teddarific <teddyni19@gmail.com>
2021-06-12 14:53:23 -0700

    [app][design] Add button

---
+++ app/src/components/design/Button.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Primary = styled.button`
+  background-color: #9086d6;
+  padding: 16px 32px;
+  font-size: 26px;
+  font-weight: 300;
+  border-radius: 10px;
+  color: white;
+  border: none;
+  cursor: pointer;
+  font-family: Roboto;
+  box-shadow: 0px 6px 4px rgba(0, 0, 0, 0.25);
+  outline: none;
+  transition: background-color 200ms;
+`;
