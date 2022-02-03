15dbbb399001207fd52c5dd040ac7c07fbc93d75
Alexander Danilowicz <alex.danilowicz@gmail.com>
2022-02-03 15:19:06 -0800

    [app] Improve Emoji Display (#187)
    * Improve Emoji Display
    
    Improving the display of emojis by using the awesome unicode-emoji package, which is basically a massive dataset of emojis and their description. We use it in the frontend to detect if an emoji version is older than 12.1. This change be changed later to detect what is on the system. Resolves #30
    
    * Add comment regarding future work on detecting which emoji version might be present

--- app/src/components/charts/WordOrEmojiCountChart.tsx
+++ app/src/components/charts/WordOrEmojiCountChart.tsx
@@ -1,11 +1,23 @@
 import log from 'electron-log';
 import React, { useEffect, useState } from 'react';
 import * as sqlite3 from 'sqlite3';
+import * as unicodeEmoji from 'unicode-emoji';
 
 import * as chatBro from '../../chatBro';
 import { IWordOrEmojiFilters } from '../../chatBro/queries/WordOrEmoji/types';
 import interpolateColors from '../../utils/colors';
 import { BarChartWrapper } from '../shared';
+
+const allEmojis = unicodeEmoji.getEmojis();
+function addDescriptionToNewerEmojis(emoji: string) {
+  const emojiData = allEmojis.find((e) => emoji === e.emoji);
+  // TODO: We should have a mapping between emoji versions and Mac OS versions.
+  // For example, I assume on the latest version of Mac OS, emojis in v13 are present.
+  if (parseFloat(emojiData.version) > 12.1) {
+    return emojiData.description;
+  }
+  return emoji;
+}
 
 interface WordOrEmojiCountProps {
   db: sqlite3.Database;
@@ -39,7 +51,9 @@ export default function WordOrEmojiCountChart(props: WordOrEmojiCountProps) {
   const COLORS = interpolateColors(words.length, colorInterpolationFunc);
 
   const data = {
-    labels: words,
+    labels: filters.isEmoji
+      ? words.map((emoji) => addDescriptionToNewerEmojis(emoji))
+      : words,
     datasets: [
       {
         label: labelText,
