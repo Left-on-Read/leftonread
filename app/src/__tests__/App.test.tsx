0e41fe4b9be734866a13c91807a7ae238127b6c5
teddarific <teddyni19@gmail.com>
2022-07-27 13:58:30 -0700

    Clean up

--- app/src/__tests__/App.test.tsx
+++ app/src/__tests__/App.test.tsx
@@ -4,8 +4,6 @@ import { render } from '@testing-library/react';
 
 import { App } from '../components/App';
 
-jest.mock('chart.js')
-
 describe('App', () => {
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
