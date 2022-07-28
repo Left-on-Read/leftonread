7cc6fd9549331bffbaaab7f652ce54f0844a23d7
teddarific <teddyni19@gmail.com>
2022-07-27 18:16:17 -0700

    Fix

--- app/src/components/Graphs/WordOrEmojiCountChart.tsx
+++ app/src/components/Graphs/WordOrEmojiCountChart.tsx
@@ -2,26 +2,12 @@ import { ipcRenderer } from 'electron';
 import log from 'electron-log';
 import { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
-import * as sqlite3 from 'sqlite3';
-import * as unicodeEmoji from 'unicode-emoji';
 
 import {
   IWordOrEmojiFilters,
-  queryEmojiOrWordCounts,
   TWordOrEmojiResults,
 } from '../../analysis/queries/WordOrEmojiQuery';
 import { interpolateColors } from '../../utils/interpolateColors';
-
-const allEmojis = unicodeEmoji.getEmojis();
-function addDescriptionToNewerEmojis(emoji: string) {
-  const emojiData = allEmojis.find((e) => emoji === e.emoji);
-  // TODO: We should have a mapping between emoji versions and Mac OS versions.
-  // For example, I assume on the latest version of Mac OS, emojis in v13 are present.
-  if (emojiData && parseFloat(emojiData.version) > 12.1) {
-    return emojiData.description;
-  }
-  return emoji;
-}
 
 export function WordOrEmojiCountChart({
   titleText,
@@ -59,9 +45,7 @@ export function WordOrEmojiCountChart({
   const COLORS = interpolateColors(words.length, colorInterpolationFunc);
 
   const data = {
-    labels: filters.isEmoji
-      ? words.map((emoji) => addDescriptionToNewerEmojis(emoji))
-      : words,
+    labels: words,
     datasets: [
       {
         label: labelText,
