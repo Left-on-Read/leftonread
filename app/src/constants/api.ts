ae6383ef896525638f8780ede9cedeca57471e32
teddarific <teddyni19@gmail.com>
2022-08-07 11:50:34 -0700

    Update api urls

--- app/src/constants/api.ts
+++ app/src/constants/api.ts
@@ -1,2 +1,4 @@
 export const API_BASE_URL =
-  process.env.NODE_ENV === 'production' ? 'TODO' : 'http://localhost:8080/api';
+  process.env.NODE_ENV === 'production'
+    ? 'https://leftonread.herokuapp.com/api'
+    : 'http://localhost:8080/api';
