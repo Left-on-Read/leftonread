8ba4cbc2ac2670c9dbcbea4404e0b934f597d4f2
teddarific <teddyni19@gmail.com>
2022-08-16 15:36:47 -0700

    Testing commit messages

--- app/release.js
+++ app/release.js
@@ -13,17 +13,17 @@ async function main() {
   }
 
   // Assert branch is up to date
-  await git.fetch('origin', currentBranch);
-  const status = await git.status();
-  if (
-    status.ahead > 0 ||
-    status.behind > 0 ||
-    status.modified.length > 0 ||
-    status.staged.length > 0
-  ) {
-    console.log('❌  Your branch is not in sync with the remote repo.');
-    return;
-  }
+  //   await git.fetch('origin', currentBranch);
+  //   const status = await git.status();
+  //   if (
+  //     status.ahead > 0 ||
+  //     status.behind > 0 ||
+  //     status.modified.length > 0 ||
+  //     status.staged.length > 0
+  //   ) {
+  //     console.log('❌  Your branch is not in sync with the remote repo.');
+  //     return;
+  //   }
 
   // Assert proper release command was passed in
   const releaseType = process.argv[2].trim();
@@ -39,42 +39,46 @@ async function main() {
   console.log(`ℹ️  Running a ${releaseType} release`);
 
   // Bump the version
-//   const bumpedVersion = await bump('./release/app/package.json', {
-//     patch: releaseType === 'patch' ? 1 : 0,
-//     minor: releaseType === 'minor' ? 1 : 0,
-//     major: releaseType === 'major' ? 1 : 0,
-//   });
+  //   const bumpedVersion = await bump('./release/app/package.json', {
+  //     patch: releaseType === 'patch' ? 1 : 0,
+  //     minor: releaseType === 'minor' ? 1 : 0,
+  //     major: releaseType === 'major' ? 1 : 0,
+  //   });
 
-//   console.log(
-//     `ℹ️  Bumped v${bumpedVersion.original} to v${bumpedVersion.updated}`
-//   );
+  //   console.log(
+  //     `ℹ️  Bumped v${bumpedVersion.original} to v${bumpedVersion.updated}`
+  //   );
 
   // Get commits since last tag
-  const commits = await git.log([`v${bumpedVersion.original}..HEAD`]);
-  console.log(commits);
+  const commits = await git.log([`v0.0.7..HEAD`]);
+  const description = commits.all
+    .map((commit) => `• ${commit.message}\n`)
+    .join()
+    .trim();
+  console.log(description);
+  //   // Add & Commit code
+  //   await git.add('.');
+  //   await git.commit(`Release v${bumpedVersion.updated}`);
 
-//   // Add & Commit code
-//   await git.add('.');
-//   await git.commit(`Release v${bumpedVersion.updated}`);
+  //   // Create a new tag
+  //   await git.addAnnotatedTag(
+  //     `v${bumpedVersion.updated}`,
+  //     `Release for ${bumpedVersion.updated}. Previous version: ${bumpedVersion.original}`
+  //   );
 
-//   // Create a new tag
-//   await git.addAnnotatedTag(
-//     `v${bumpedVersion.updated}`,
-//     `Release for ${bumpedVersion.updated}. Previous version: ${bumpedVersion.original}`
-//   );
+  //   console.log(`ℹ️  Created new tag v${bumpedVersion.updated}`);
 
-//   console.log(`ℹ️  Created new tag v${bumpedVersion.updated}`);
+  //   console.log(`ℹ️  Pushing updates to origin...`);
 
-//   console.log(`ℹ️  Pushing updates to origin...`);
+  //   // Push to origin
+  //   await git.push('origin');
+  //   await git.push(['origin', '--tags']);
 
-//   // Push to origin
-//   await git.push('origin');
-//   await git.push(['origin', '--tags']);
+  //   // Pull from origin
+  //   await git.fetch('origin');
 
-//   // Pull from origin
-//   await git.fetch('origin');
-
-//   console.log(`🚀  Success!`);
-// }
+  //   console.log(`🚀  Success!`);
+  // }
+}
 
 main();
