abd02bdd0328654a99d9d224c1e0148c60d31efb
teddarific <teddyni19@gmail.com>
2022-08-16 13:20:51 -0700

    Release script

--- app/release.js
+++ app/release.js
@@ -1,22 +1,70 @@
+/* eslint-disable */
 const simpleGit = require('simple-git');
+const bump = require('json-bump');
 
 async function main() {
   const git = simpleGit();
-
   const currentBranch = (await git.branchLocal()).current;
 
   // Assert on main branch
-  //   if (currentBranch !== 'main') {
-  //     console.log('❌  You must be on the main branch to release.');
-  //     return;
-  //   }
+  if (currentBranch !== 'main') {
+    console.log('❌  You must be on the main branch to release.');
+    return;
+  }
 
   // Assert branch is up to date
   await git.fetch('origin', currentBranch);
   const status = await git.status();
-  if (status.ahead > 0 || status.behind > 0) {
+  if (
+    status.ahead > 0 ||
+    status.behind > 0 ||
+    status.modified.length > 0 ||
+    status.staged.length > 0
+  ) {
     console.log('❌  Your branch is not in sync with the remote repo.');
+    return;
   }
+
+  // Assert proper release command was passed in
+  const releaseType = process.argv[2].trim();
+  if (
+    releaseType !== 'patch' &&
+    releaseType !== 'minor' &&
+    releaseType !== 'major'
+  ) {
+    console.log('❌  Must specify a release type: patch, minor, major');
+    return;
+  }
+
+  console.log(`ℹ️  Running a ${releaseType} release`);
+
+  // Bump the version
+  const bumpedVersion = await bump('./release/app/package.json', {
+    patch: releaseType === 'patch' ? 1 : 0,
+    minor: releaseType === 'minor' ? 1 : 0,
+    major: releaseType === 'major' ? 1 : 0,
+  });
+
+  console.log(
+    `ℹ️  Bumped v${bumpedVersion.original} to v${bumpedVersion.updated}`
+  );
+
+  // Create a new tag
+  await git.addAnnotatedTag(
+    `v${bumpedVersion.updated}`,
+    `Release for ${bumpedVersion.updated}. Previous version: ${bumpedVersion.original}`
+  );
+
+  console.log(`ℹ️  Created new tag v${bumpedVersion.updated}`);
+  console.log(`ℹ️  Pushing updates to origin...`);
+
+  // Push to origin
+  await git.pushTags('origin');
+
+  // Pull from origin
+  await git.fetch('origin');
+
+  console.log(`🚀  Success!`);
 }
 
 main();
