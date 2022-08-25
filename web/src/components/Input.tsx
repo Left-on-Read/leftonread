28d0cf677d1a5a71e5dc9912e8173c2c2eb0b635
teddarific <teddyni19@gmail.com>
2022-08-24 23:43:55 -0700

    Ok now fix

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -17,27 +17,6 @@ export default function Input({
   return (
     <input
       className={className}
-      css={{
-        padding: '8px 12px',
-        fontWeight: 300,
-        borderRadius: '10px',
-        color: 'black',
-        border: `1px solid ${Theme.secondary.main}`,
-        fontFamily: 'Roboto',
-        outline: 'none',
-        backgroundColor: '#F1F1F1',
-        transition: 'border-color 200ms',
-        '&:focus': {
-          borderWidth: `2px`,
-          borderColor: Theme.secondary.main,
-        },
-        '&:hover': {
-          borderColor: Theme.secondary.main,
-        },
-        [belowBreakpoint.md]: {
-          padding: '8px 8px',
-        },
-      }}
       placeholder={placeholder}
       value={value}
       onChange={(e) => {
