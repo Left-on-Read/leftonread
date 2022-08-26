9b4f7200f22ce328ab647221e87e0248bea1988e
alexdanilowicz <alex.danilowicz@gmail.com>
2022-08-25 19:34:26 -0700

    Stacked group chat v2

--- app/src/analysis/queries/TopFriendsQuery.ts
+++ app/src/analysis/queries/TopFriendsQuery.ts
@@ -9,54 +9,58 @@ import {
 } from './filters/sharedQueryFilters';
 
 interface ITopFriendsChartData {
+  count: number;
   friend: string;
-  total: number;
-  sent: number;
-  received: number;
+  is_from_me: string; // in reality these can be coerced into bools
+  group_chat: string; // in reality these can be coerced into bools
 }
 
 export type TTopFriendsResults = ITopFriendsChartData[];
 
-enum TopFriendsColumns {
-  COUNT = 'count',
-  PHONE_NUMBER = 'phone_number',
-  FRIEND = 'friend',
-  IS_FROM_ME = 'sent',
-  TEXT = 'text',
-}
+// enum TopFriendsColumns {
+//   COUNT = 'count',
+//   PHONE_NUMBER = 'phone_number',
+//   FRIEND = 'friend',
+//   IS_FROM_ME = 'sent',
+//   TEXT = 'text',
+// }
 
-enum TopFriendsOutputColumns {
-  FRIEND = 'friend',
-  TOTAL = 'total',
-  SENT = 'sent',
-  RECEIVED = 'received',
-}
+// enum TopFriendsOutputColumns {
+//   FRIEND = 'friend',
+//   TOTAL = 'total',
+//   SENT = 'sent',
+//   RECEIVED = 'received',
+//   group_chat = 'group_chat',
+// }
 
 const getCoreQuery = (allFilters: string) => {
-  return `SELECT
+  return `
+SELECT
 COUNT(*) as count,
 id as phone_number,
 COALESCE(contact_name, id, contact_name_with_group_chat_participants_populated) as friend,
-is_from_me as sent
+CASE WHEN (is_from_me IS 1) THEN "true" ELSE "false" END AS is_from_me,
+CASE WHEN (room_name IS NULL) THEN "false" ELSE "true" END AS group_chat
 FROM ${CoreTableNames.CORE_MAIN_TABLE}
 -- NOTE: filters should always be applied as earliest as possible
 ${allFilters}
-GROUP BY id, is_from_me
+GROUP BY COALESCE(contact_name, id, contact_name_with_group_chat_participants_populated), is_from_me
 `;
 };
 
-function getSentOrReceived(
-  alias: string,
-  isFromMe: boolean,
-  allFilters: string
-) {
-  return `
-  SELECT
-    count AS ${alias},
-    friend
-  FROM (${getCoreQuery(allFilters)})
-  WHERE sent = ${isFromMe ? '1' : '0'}`;
-}
+// function getSentOrReceived(
+//   alias: string,
+//   isFromMe: boolean,
+//   allFilters: string
+// ) {
+//   return `
+//   SELECT
+//     count AS ${alias},
+//     friend,
+//     group_chat
+//   FROM (${getCoreQuery(allFilters)})
+//   WHERE sent = ${isFromMe ? '1' : '0'}`;
+// }
 
 export async function queryTopFriends(
   db: sqlite3.Database,
@@ -64,38 +68,40 @@ export async function queryTopFriends(
 ): Promise<TTopFriendsResults> {
   const limit = filters.limit || DEFAULT_FILTER_LIMIT;
   const allFilters = getAllFilters(filters);
-  const query = `
-  WITH SENT_TABLE AS (
-    ${getSentOrReceived('sent', true, allFilters)}
-  ),
-  RECEIVED_TABLE AS (
-    ${getSentOrReceived('received', false, allFilters)}
-  ),
-  COMBINED_TABLE AS (
-    SELECT
-      sent + received as ${TopFriendsOutputColumns.TOTAL},
-      -- NOTE: sent table has the group chat messages
-      SENT_TABLE.friend as ${TopFriendsOutputColumns.FRIEND},
-      sent as ${TopFriendsOutputColumns.SENT},
-      received as ${TopFriendsOutputColumns.RECEIVED}
-    FROM
-      -- TODO: I think because we are RIGHT JOIN if you only 
-      -- receieve and don't send then that's a problem
-      RECEIVED_TABLE
-    RIGHT JOIN
-      SENT_TABLE
-    ON
-      RECEIVED_TABLE.${TopFriendsColumns.FRIEND} = SENT_TABLE.${
-    TopFriendsColumns.FRIEND
-  }
-  )
-  SELECT
-    ${Object.keys(TopFriendsOutputColumns).join(', ')}
-  FROM
-    COMBINED_TABLE
-  ORDER BY ${TopFriendsOutputColumns.TOTAL} DESC
-  LIMIT ${limit}
-  `;
+  // const query = `
+  // WITH SENT_TABLE AS (
+  //   ${getSentOrReceived('sent', true, allFilters)}
+  // ),
+  // RECEIVED_TABLE AS (
+  //   ${getSentOrReceived('received', false, allFilters)}
+  // ),
+  // COMBINED_TABLE AS (
+  //   SELECT
+  //     sent + received as ${TopFriendsOutputColumns.TOTAL},
+  //     -- NOTE: sent table has the group chat messages
+  //     SENT_TABLE.friend as ${TopFriendsOutputColumns.FRIEND},
+  //     sent as ${TopFriendsOutputColumns.SENT},
+  //     received as ${TopFriendsOutputColumns.RECEIVED},
+  //     SENT_TABLE.group_chat as group_chat
+  //   FROM
+  //     -- TODO: I think because we are RIGHT JOIN if you only
+  //     -- receieve and don't send then that's a problem
+  //     RECEIVED_TABLE
+  //   RIGHT JOIN
+  //     SENT_TABLE
+  //   ON
+  //     RECEIVED_TABLE.${TopFriendsColumns.FRIEND} = SENT_TABLE.${
+  //   TopFriendsColumns.FRIEND
+  // }
+  // )
+  // SELECT
+  //   ${Object.keys(TopFriendsOutputColumns).join(', ')}
+  // FROM
+  //   COMBINED_TABLE
+  // ORDER BY ${TopFriendsOutputColumns.TOTAL} DESC
+  // LIMIT ${limit}
+  // `;
+  const query = getCoreQuery(allFilters);
 
   return sqlite3Wrapper.allP(db, query);
 }
--- app/src/components/Graphs/TopFriendsChart.tsx
+++ app/src/components/Graphs/TopFriendsChart.tsx
@@ -1,4 +1,5 @@
 import { Spinner, Text, theme as defaultTheme } from '@chakra-ui/react';
+import { GroupChatFilters } from 'constants/filters';
 import { ipcRenderer } from 'electron';
 import log from 'electron-log';
 import { useEffect, useRef, useState } from 'react';
@@ -25,6 +26,8 @@ export function TopFriendsChart({
   const [friends, setFriends] = useState<string[]>([]);
   const [received, setReceived] = useState<number[]>([]);
   const [sent, setSent] = useState<number[]>([]);
+  const [groupChatSent, setGroupChatSent] = useState<number[]>([]);
+  const [groupChatReceived, setGroupChatReceived] = useState<number[]>([]);
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<null | string>(null);
@@ -38,9 +41,93 @@ export function TopFriendsChart({
           'query-top-friends',
           filters
         );
-        setFriends(topFriendsDataList.map((obj) => obj.friend));
-        setSent(topFriendsDataList.map((obj) => obj.sent));
-        setReceived(topFriendsDataList.map((obj) => obj.received));
+        console.log(topFriendsDataList);
+        // Example data
+        // [
+        //  {count: 6, friend: 'Alex Danilowicz', is_from_me: 'false', group_chat: 'true'}
+        //  {count: 13, friend: 'Alex Danilowicz', is_from_me: 'true', group_chat: 'false'}
+        //  {count: 4, friend: 'Teddy Ni', is_from_me: 'false', group_chat: 'true'}
+        //  {count: 1, friend: 'Teddy Ni', is_from_me: 'true', group_chat: 'false'}
+        //  {count: 2, friend: 'Teddy Ni,Alex Danilowicz', is_from_me: 'true', group_chat: 'true'}
+        // ]
+        // setFriends(topFriendsDataList.map((obj) => obj.friend));
+        // setSent(topFriendsDataList.map((obj) => obj.sent));
+        // setReceived(topFriendsDataList.map((obj) => obj.received));
+
+        // construct totals
+        const totalByFriendRegardlessOfChat: Record<string, number> = {};
+
+        const totalByFriendNotGroupChatSent: Record<string, number> = {};
+        const totalByFriendNotGroupChatReceived: Record<string, number> = {};
+
+        const totalByFriendGroupChatSent: Record<string, number> = {};
+        const totalByFriendGroupChatReceived: Record<string, number> = {};
+
+        // Split the friend: 'Teddy Ni,Alex Danilowicz' values into seperate rows
+        const topFriendsDataListFinal: TTopFriendsResults = [];
+        topFriendsDataList.forEach((obj) => {
+          // if a contact has a comma in, then this will break...
+          if (obj.friend.includes(',') && obj.group_chat === 'true') {
+            obj.friend.split(',').forEach((f) => {
+              topFriendsDataListFinal.push({
+                ...obj,
+                friend: f,
+              });
+            });
+          } else {
+            topFriendsDataListFinal.push(obj);
+          }
+        });
+        console.log(topFriendsDataListFinal);
+
+        topFriendsDataListFinal.forEach((obj) => {
+          if (!totalByFriendRegardlessOfChat[obj.friend]) {
+            totalByFriendRegardlessOfChat[obj.friend] = 0;
+          }
+
+          if (!totalByFriendNotGroupChatSent[obj.friend]) {
+            totalByFriendNotGroupChatSent[obj.friend] = 0;
+          }
+
+          if (!totalByFriendNotGroupChatReceived[obj.friend]) {
+            totalByFriendNotGroupChatReceived[obj.friend] = 0;
+          }
+
+          if (!totalByFriendGroupChatSent[obj.friend]) {
+            totalByFriendGroupChatSent[obj.friend] = 0;
+          }
+
+          if (!totalByFriendGroupChatReceived[obj.friend]) {
+            totalByFriendGroupChatReceived[obj.friend] = 0;
+          }
+
+          totalByFriendRegardlessOfChat[obj.friend] += obj.count;
+          if (obj.group_chat === 'false') {
+            if (obj.is_from_me === 'true') {
+              totalByFriendNotGroupChatSent[obj.friend] += obj.count;
+            } else {
+              totalByFriendNotGroupChatReceived[obj.friend] += obj.count;
+            }
+          } else if (obj.group_chat === 'true') {
+            if (obj.is_from_me === 'true') {
+              totalByFriendGroupChatSent[obj.friend] += obj.count;
+            } else {
+              totalByFriendGroupChatReceived[obj.friend] += obj.count;
+            }
+          }
+        });
+
+        const sortedByTotal = Object.fromEntries(
+          Object.entries(totalByFriendRegardlessOfChat).sort(
+            ([, a], [, b]) => b - a
+          )
+        );
+        console.log(sortedByTotal);
+        setFriends(Object.keys(sortedByTotal));
+        setSent(Object.values(totalByFriendNotGroupChatSent));
+        setReceived(Object.values(totalByFriendNotGroupChatReceived));
+        setGroupChatReceived(Object.values(totalByFriendGroupChatReceived));
+        setGroupChatSent(Object.values(totalByFriendGroupChatSent));
       } catch (err: unknown) {
         if (err instanceof Error) {
           setError(err.message);
@@ -56,6 +143,15 @@ export function TopFriendsChart({
   const data = {
     labels: friends,
     datasets: [
+      filters.groupChat === GroupChatFilters.BOTH
+        ? {
+            label: 'Group Chat Sent',
+            data: groupChatSent,
+            backgroundColor: defaultTheme.colors.blue['600'],
+            borderColor: defaultTheme.colors.blue['900'],
+            borderRadius: 5,
+          }
+        : { label: '', backgroundColor: 'white' },
       {
         label: 'Sent',
         data: sent,
@@ -70,6 +166,15 @@ export function TopFriendsChart({
         borderColor: defaultTheme.colors.gray['500'],
         borderRadius: 5,
       },
+      filters.groupChat === GroupChatFilters.BOTH
+        ? {
+            label: 'Group Chat Received',
+            data: groupChatReceived,
+            backgroundColor: defaultTheme.colors.gray['600'],
+            borderColor: defaultTheme.colors.gray['900'],
+            borderRadius: 5,
+          }
+        : { label: '', backgroundColor: 'white' },
     ],
   };
 
@@ -79,11 +184,13 @@ export function TopFriendsChart({
         ticks: {
           precision: 0,
         },
+        stacked: true,
       },
       xAxis: {
         ticks: {
           precision: 0,
         },
+        stacked: true,
       },
     },
   };
