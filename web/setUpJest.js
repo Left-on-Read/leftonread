1a9c3dd4e0930f9fbf69a9245dec12f92927edbf
alexdanilowicz <alex.danilowicz@gmail.com>
2022-09-13 20:27:39 -0700

    Fix tests

--- web/setUpJest.js
+++ web/setUpJest.js
@@ -5,3 +5,12 @@ jest.mock('./src/utils/gtag')
 if (!SVGElement.prototype.getTotalLength) {
   SVGElement.prototype.getTotalLength = () => 1
 }
+
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    }
+  }
