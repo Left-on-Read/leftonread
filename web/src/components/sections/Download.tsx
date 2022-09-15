435d651661350d6085220c10756ced5dfa4a0639
teddarific <teddyni19@gmail.com>
2022-09-15 15:09:36 -0700

    Add fb messenger waitlist

--- web/src/components/sections/Download.tsx
+++ web/src/components/sections/Download.tsx
@@ -1,9 +1,15 @@
 import { DownloadIcon } from '@chakra-ui/icons'
-import { Box, Button, Text } from '@chakra-ui/react'
+import { Box, Button, FormControl, Icon, Input, Text } from '@chakra-ui/react'
+import { useState } from 'react'
+import { FaFacebookMessenger } from 'react-icons/fa'
+import { FiCheckCircle } from 'react-icons/fi'
 
 import { LATEST_APP_VERSION_FOR_MARKETING_SITE } from '../../constants/APP_VERSION'
+import { addContact } from '../../utils/addContact'
 import { logEvent } from '../../utils/gtag'
+import { isValidEmail } from '../../utils/validation'
 import { DefaultContentContainer } from '../DefaultContentContainer'
+import { StatusLoaderState } from '../StatusLoader'
 
 export const handleDownload = () => {
   logEvent({
@@ -18,6 +24,38 @@ export function Download({
 }: {
   ctaRef: React.RefObject<HTMLDivElement>
 }) {
+  const [email, setEmail] = useState<string>('')
+  const [state, setState] = useState<StatusLoaderState | null>(null)
+
+  const signUpEmail = async (submittedEmail: string) => {
+    if (!isValidEmail(submittedEmail)) {
+      setState('error')
+      // setMessage('Please enter a valid email.')
+      return
+    }
+
+    setState('loading')
+    // setMessage(undefined)
+
+    try {
+      await addContact({
+        email: submittedEmail,
+        type: 'FB_MESSENGER_WAITLIST',
+      })
+      setState('success')
+      // setMessage('Successfully signed up!')
+    } catch (e) {
+      console.error(e)
+      setState('error')
+      // setMessage('Uh oh, something went wrong.')
+    }
+  }
+
+  const handleSubmitNotify = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    signUpEmail(email)
+  }
+
   return (
     <Box
       style={{
@@ -86,6 +124,164 @@ export function Download({
               >
                 Download for Mac
               </Button>
+            </Box>
+          </Box>
+          <Box
+            style={{
+              backgroundColor: 'white',
+              borderRadius: 32,
+              display: 'flex',
+              justifyContent: 'space-between',
+              marginBottom: 120,
+            }}
+            margin={{
+              base: '0',
+              lg: '8%',
+            }}
+            padding={{
+              base: '10%',
+              md: '5%',
+              lg: '5%',
+            }}
+            bgGradient="linear(to-tr, #0695FF, #A334FA, #FF6968)"
+            flexDirection={{
+              base: 'column',
+              lg: 'row',
+            }}
+          >
+            <Box width={{ base: '100%' }}>
+              <Box
+                style={{
+                  lineHeight: 1.3,
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <Box>
+                  <div
+                    style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Icon
+                      as={FaFacebookMessenger}
+                      color="white"
+                      fontSize={{
+                        base: 'xl',
+                        md: '2xl',
+                        lg: '3xl',
+                      }}
+                      style={{ marginRight: 12 }}
+                    />
+                    <Text
+                      color="white"
+                      fontSize={{
+                        base: 'xl',
+                        md: '2xl',
+                        lg: '3xl',
+                      }}
+                      fontWeight="extrabold"
+                    >
+                      Get Early Access to Messenger
+                    </Text>
+                  </div>
+
+                  <Text
+                    color="white"
+                    fontSize={{
+                      base: 'md',
+                      lg: 'xl',
+                    }}
+                    marginTop={{
+                      base: 4,
+                      lg: 8,
+                    }}
+                  >
+                    {`We're working on bringing the platform you know and love to FB Messenger.`}
+                  </Text>
+                  <Text
+                    color="white"
+                    fontSize={{
+                      base: 'md',
+                      lg: 'xl',
+                    }}
+                    marginTop={{
+                      base: 2,
+                      lg: 4,
+                    }}
+                  >
+                    Sign up here to be notified for early access!
+                  </Text>
+                  <Box style={{ marginTop: 22, width: '80%' }}>
+                    <form onSubmit={handleSubmitNotify}>
+                      <Box
+                        style={{
+                          display: 'flex',
+                        }}
+                        flexDirection={{
+                          base: 'column',
+                          md: 'row',
+                        }}
+                        alignItems={{
+                          base: 'flex-start',
+                          md: 'center',
+                        }}
+                      >
+                        <FormControl>
+                          <Input
+                            size="md"
+                            placeholder={'you@example.com'}
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            disabled={
+                              state === 'loading' || state === 'success'
+                            }
+                            focusBorderColor={'gray.200'}
+                            isInvalid={state === 'error'}
+                            style={{
+                              borderRadius: 8,
+                              backgroundColor: 'white',
+                            }}
+                          />
+                        </FormControl>
+                        <Button
+                          isLoading={state === 'loading'}
+                          loadingText="Submitting..."
+                          disabled={state === 'loading' || state === 'success'}
+                          type="submit"
+                          size="md"
+                          style={{
+                            padding: '15px 25px',
+                            transition: '.25s',
+                          }}
+                          marginLeft={{
+                            base: '0px',
+                            md: '10px',
+                          }}
+                          marginTop={{
+                            base: '8px',
+                            md: '0px',
+                          }}
+                          leftIcon={
+                            state === 'success' ? (
+                              <Icon as={FiCheckCircle} color="green.600" />
+                            ) : undefined
+                          }
+                        >
+                          <span className="primary">
+                            {state === 'success' ? (
+                              <>Success</>
+                            ) : (
+                              <>Get Notified</>
+                            )}
+                          </span>
+                        </Button>
+                      </Box>
+                    </form>
+                  </Box>
+                </Box>
+              </Box>
             </Box>
           </Box>
           {/* <Box
