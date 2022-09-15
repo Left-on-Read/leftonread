c599d23a69d330f2419d280ca13395ec7441836b
alexdanilowicz <alex.danilowicz@gmail.com>
2022-09-15 09:25:33 -0700

    Block Device Id

--- app/src/utils/amplitudeClient.ts
+++ app/src/utils/amplitudeClient.ts
@@ -4,16 +4,11 @@ import { machineIdSync } from 'node-machine-id';
 
 import { getUuid } from './store';
 
-const devRegistrationIds = [
-  '4ebffdd19ef0686028071bd7e4d41e00ab70636404cb195a9896373ec0d1346c',
-  '46283f3b205c830217d6d8bbc88c134ecd80030854fe2143acb5c89dcb8a851',
-];
-
 const regristrationId = machineIdSync(false);
-const index = devRegistrationIds.findIndex((e) => e === regristrationId);
-
 export const AmplitudeClient =
-  process.env.NODE_ENV === 'production' && index === -1
+  process.env.NODE_ENV === 'production' &&
+  regristrationId !==
+    '4ebffdd19ef0686028071bd7e4d41e00ab70636404cb195a9896373ec0d1346c'
     ? Amplitude.init('5fdd650d1d9fa3b33f41029f44b188c3')
     : {
         logEvent: () => {},
