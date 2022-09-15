865b44f40e998d09eca96e69de78403a0b4c58bb
Alexander Danilowicz <alex.danilowicz@gmail.com>
2022-09-15 09:26:38 -0700

    Block Amplitude Logging if our own machine IDs (#376)

--- app/src/utils/amplitudeClient.ts
+++ app/src/utils/amplitudeClient.ts
@@ -1,10 +1,14 @@
 import * as Amplitude from '@amplitude/node';
 import log from 'electron-log';
+import { machineIdSync } from 'node-machine-id';
 
 import { getUuid } from './store';
 
+const regristrationId = machineIdSync(false);
 export const AmplitudeClient =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'production' &&
+  regristrationId !==
+    '4ebffdd19ef0686028071bd7e4d41e00ab70636404cb195a9896373ec0d1346c'
     ? Amplitude.init('5fdd650d1d9fa3b33f41029f44b188c3')
     : {
         logEvent: () => {},
