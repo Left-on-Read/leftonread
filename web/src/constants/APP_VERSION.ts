bcb40042bab5ede5b927bd3242f2cefa032e08da
teddarific <teddyni19@gmail.com>
2022-09-18 21:50:25 -0700

    Release v1.2.5

--- web/src/constants/APP_VERSION.ts
+++ web/src/constants/APP_VERSION.ts
@@ -1,2 +1,2 @@
 /* This file is auto-edited, please do not edit it or move it unless you are touching release pipelines */
-export const LATEST_APP_VERSION_FOR_MARKETING_SITE = '1.2.4'
+export const LATEST_APP_VERSION_FOR_MARKETING_SITE = '1.2.5'
