d2ce19b5ea77516709568325c7b152e4f5edeab6
teddarific <teddyni19@gmail.com>
2022-09-22 15:54:32 -0700

    Update

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -38,32 +38,6 @@ class MyDocument extends Document {
           `,
             }}
           />
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1130957880846683');
-              fbq('track', 'PageView');
-          `,
-            }}
-          />
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `
-            <img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=1130957880846683&ev=PageView&noscript=1"
-            />
-            `,
-            }}
-          />
         </Head>
         <body>
           <Main />
