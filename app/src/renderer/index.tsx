cc6b40b1ea0e0b192e582831cb0b5a0518d7ebdb
Alexander Danilowicz <alex.danilowicz@gmail.com>
2022-10-27 20:40:45 -0700

    Fix Palette error (#448)

--- app/src/renderer/index.tsx
+++ app/src/renderer/index.tsx
@@ -12,52 +12,50 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from '../components/App';
 
-if (process.env.NODE_ENV === 'production') {
-  init({
-    key: 'cl8j2xvzc014708mq30m2vaqv',
-    plugins: [events(), vitals(), network(), measure(), profiler()],
-  });
+init({
+  key: 'cl8j2xvzc014708mq30m2vaqv',
+  plugins: [events(), vitals(), network(), measure(), profiler()],
+});
 
-  // Profile page load
-  profiler.start({ sampleInterval: 10, maxBufferSize: 10_000 });
-  addEventListener('load', () => profiler.stop());
+// Profile page load
+profiler.start({ sampleInterval: 10, maxBufferSize: 10_000 });
+addEventListener('load', () => profiler.stop());
 
-  // A utility for profiling and label frequent events
-  const debounce = (start: any, stop: any, opts = { timeout: 1_000 }) => {
-    let timeoutId: any;
-    return () => {
-      if (timeoutId === undefined) {
-        start();
-      } else {
-        clearTimeout(timeoutId);
-      }
-      // Debounce marking the end of the label
-      timeoutId = setTimeout(() => {
-        stop();
-        timeoutId = undefined;
-      }, opts.timeout);
-    };
-  };
-  // Debounce starting the profiler
-  const debounceProfiler = debounce(
-    () => {
-      label.start('ui.interaction');
-      profiler.start({
-        sampleInterval: 10,
-        maxBufferSize: 10_000,
-      });
-    },
-    () => {
-      label.end('ui.interaction');
-      return profiler.stop();
+// A utility for profiling and label frequent events
+const debounce = (start: any, stop: any, opts = { timeout: 1_000 }) => {
+  let timeoutId: any;
+  return () => {
+    if (timeoutId === undefined) {
+      start();
+    } else {
+      clearTimeout(timeoutId);
     }
-  );
-  // Profile scroll, mousemove, and click events
-  addEventListener('wheel', debounceProfiler);
-  addEventListener('mousemove', debounceProfiler);
-  addEventListener('click', debounceProfiler);
-  addEventListener('keypress', debounceProfiler);
-}
+    // Debounce marking the end of the label
+    timeoutId = setTimeout(() => {
+      stop();
+      timeoutId = undefined;
+    }, opts.timeout);
+  };
+};
+// Debounce starting the profiler
+const debounceProfiler = debounce(
+  () => {
+    label.start('ui.interaction');
+    profiler.start({
+      sampleInterval: 10,
+      maxBufferSize: 10_000,
+    });
+  },
+  () => {
+    label.end('ui.interaction');
+    return profiler.stop();
+  }
+);
+// Profile scroll, mousemove, and click events
+addEventListener('wheel', debounceProfiler);
+addEventListener('mousemove', debounceProfiler);
+addEventListener('click', debounceProfiler);
+addEventListener('keypress', debounceProfiler);
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
