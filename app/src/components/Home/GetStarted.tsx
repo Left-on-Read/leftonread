867b939a3d95a5bd1ee25e685b0614ce02709d15
Alexander Danilowicz <alex.danilowicz@gmail.com>
2022-11-28 22:22:12 -0800

    Revert "🐛: fix check permissions hanging for some users by unpromisifying it (#472)"
    This reverts commit b0c8420844b218eaa0bddc6ce73b9b43b125548e.

--- app/src/components/Home/GetStarted.tsx
+++ app/src/components/Home/GetStarted.tsx
@@ -1,12 +1,10 @@
 import { ArrowForwardIcon, Icon } from '@chakra-ui/icons';
 import { Button, Text } from '@chakra-ui/react';
 import { ipcRenderer } from 'electron';
-import * as fs from 'fs';
 import { useState } from 'react';
 import { IconType } from 'react-icons';
 import { FiBarChart2, FiGlobe, FiLock, FiShield } from 'react-icons/fi';
 
-import { chatPaths } from '../../analysis/directories';
 import { logEvent } from '../../utils/analytics';
 
 function BulletPoint({
@@ -51,22 +49,12 @@ export function GetStarted({ onNext }: { onNext: (arg0: boolean) => void }) {
 
   const onStart = async () => {
     setIsLoading(true);
-    console.log('about to check access');
-    let hasAccess = false;
-    try {
-      console.log('inside try');
-      fs.accessSync(chatPaths.original, fs.constants.R_OK);
-      console.log('checking 2nd access sync');
-      fs.accessSync(`${process.env.HOME}`, fs.constants.W_OK);
-      console.log('passed permissions check');
-      hasAccess = true;
-    } catch (e: unknown) {
-      console.log(`Failed permissions check: ${e}`);
-      hasAccess = false;
-    }
-
+    const hasAccess = await ipcRenderer.invoke('check-permissions');
     logEvent({
       eventName: 'GET_STARTED',
+      properties: {
+        hasAccess,
+      },
     });
     onNext(hasAccess);
     setIsLoading(false);
