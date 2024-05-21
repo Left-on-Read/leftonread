229f20c01e0e570719eaa0066dbc0cfb546925eb
alexdanilowicz <alex.danilowicz@gmail.com>
2024-05-20 19:08:08 -0600

    Deploy server

--- server/src/emailTemplate.ts
+++ server/src/emailTemplate.ts
@@ -1,6 +1,5 @@
 export const getEmailTemplate = (licenseKey: string) => {
   const backgroundColor = '#FFFFFF'
-
   return `
         <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
